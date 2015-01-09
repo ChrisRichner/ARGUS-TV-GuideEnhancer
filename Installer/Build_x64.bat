3e444d8546f87526e6142a78d2642ccb501d338d
Christoph Richner <christoph.richner@outlook.com>
2015-01-09 21:32:19 +0000

    Removed old installer

--- Installer/Build_x64.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-REM This will build the wix project for default Wix install path on x64 Windows
-msbuild Installer.wixproj /p:WixTargetsPath="C:\Program Files (x86)\MSBuild\Microsoft\WiX\v3.x\wix.targets";Configuration=Release;WixToolPath="C:\Program Files (x86)\WiX Toolset v3.8\bin";WixTasksPath="C:\Program Files (x86)\WiX Toolset v3.8\bin\WixTasks.dll"
