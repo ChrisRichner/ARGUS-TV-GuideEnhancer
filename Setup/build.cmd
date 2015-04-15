7df1f0db7be6636695f3b9605877a51a0f49d034
Chris Richner <christoph.richner@gmail.com>
2015-04-15 22:52:40 +0200

    Init v2.3.4 release build now generates a msi file

--- Setup/build.cmd
+++ Setup/build.cmd
@@ -1,3 +1,4 @@
-set SOURCEDIR=%CD%
+set SOURCEDIR=%1
+cd SOURCEDIR
 "%WIX%bin\candle.exe" -nologo "%SOURCEDIR%\Product.wxs" -out "%SOURCEDIR%\Setup.wixobj"  -ext WixUIExtension -ext WixNetFxExtension
-"%WIX%bin\light.exe" -nologo "%SOURCEDIR%\Setup.wixobj" -out "%SOURCEDIR%\GuideEnricher.msi"  -ext WixUIExtension -ext WixNetFxExtension
+"%WIX%bin\light.exe" -b %1 -nologo "%SOURCEDIR%\Setup.wixobj" -out "%SOURCEDIR%\GuideEnricherInstall-v%2.msi"  -ext WixUIExtension -ext WixNetFxExtension
