84a59feae571aa01797929cc1f401a76c0f721a0
Christoph Richner <christoph.richner@outlook.com>
2015-07-17 09:56:10 +0200

    WIX changes

--- Setup/build.cmd
+++ Setup/build.cmd
@@ -1,4 +1,4 @@
 set SOURCEDIR=%1
 cd SOURCEDIR
-"C:\Program Files (x86)\WiX Toolset v3.9\bin\candle.exe" -nologo "%SOURCEDIR%\Product.wxs" -out "%SOURCEDIR%\Setup.wixobj"  -ext WixUIExtension -ext WixNetFxExtension
-"C:\Program Files (x86)\WiX Toolset v3.9\bin\light.exe" -b %1 -nologo "%SOURCEDIR%\Setup.wixobj" -out "%SOURCEDIR%\GuideEnricherInstall-v%2.msi"  -ext WixUIExtension -ext WixNetFxExtension
+"%WIX%\bin\candle.exe" -nologo "%SOURCEDIR%\Product.wxs" -out "%SOURCEDIR%\Setup.wixobj"  -ext WixUIExtension -ext WixNetFxExtension
+"%WIX%\bin\light.exe" -b %1 -nologo "%SOURCEDIR%\Setup.wixobj" -out "%SOURCEDIR%\bin\GuideEnricherInstall-v%2.msi"  -ext WixUIExtension -ext WixNetFxExtension
