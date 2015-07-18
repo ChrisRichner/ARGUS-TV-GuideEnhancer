86a7d8702dbaa8b3b77dc239b9469bb2d5ad540b
Christoph Richner <christoph.richner@outlook.com>
2015-07-18 22:44:51 +0200

    msi version == build number

--- Setup/build.cmd
+++ Setup/build.cmd
@@ -1,4 +1,5 @@
 set SOURCEDIR=%1
+set productversion=%2
 cd SOURCEDIR
 "%WIX%\bin\candle.exe" -nologo "%SOURCEDIR%\Product.wxs" -out "%SOURCEDIR%\Setup.wixobj"  -ext WixUIExtension -ext WixNetFxExtension
 "%WIX%\bin\light.exe" -b %1 -nologo "%SOURCEDIR%\Setup.wixobj" -out "%SOURCEDIR%\bin\GuideEnricherInstall-v%2.msi"  -ext WixUIExtension -ext WixNetFxExtension
